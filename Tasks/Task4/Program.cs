fbf511c014a43c1cc3f65d29415349eea6029caa
kaushikgub <kaushik.gub@gmail.com>
2021-08-31 22:42:11 +0600

    task 4

--- Tasks/Task4/Program.cs
+++ Tasks/Task4/Program.cs
@@ -12,8 +12,26 @@ namespace Task4
 
         public static int BinaryToDecimal(string binaryNumber)
         {
+            try
+            {
+                nint decimalValue = 0;
+                nint number = nint.Parse(binaryNumber);
+                int bValue = 1;
+                while (number > 0)
+                {
+                    nint reminder = number % 10;
+                    number = number / 10;
+                    decimalValue += reminder * bValue;
+                    bValue = bValue * 2;
+                }
+                return (int)decimalValue;
+            }
+            catch
+            {
+                throw new NotImplementedException();
+            }
+
             // Write your code here
-            throw new NotImplementedException();
         }
     }
 }
